9ce49d4209f84e9fad0f96a68b8d51300bb22775
srcrowl <sc8wf@virginia.edu>
2022-11-27 20:13:45 -0500

    Added function to generate annotated canonical protein sequence

--- ExonPTMapper/plot.py
+++ ExonPTMapper/plot.py
@@ -103,7 +103,128 @@ class plotter:
         ax.set_title(self.genes.loc[gene_id, 'Gene name'])
         ax.axis('off')
         
+    def annotateProteinSequence(self, protein_id = None, gene_name = None,include_exons = True,include_domains = False, num_aa_per_row = 100, figwidth = 20):
+        """
+        Print an annotated version of the canonical amino acid sequence of a given gene/protein. Indicate which residues are PTMs and where different splice boundaries lie on the protein. If indicated, include exon ID information or location of domains in the protein sequence.
+        """
+        #given protein id, find exons
+        if protein_id is not None:
+            trans = self.proteins.loc[protein_id, 'Canonical Transcripts']
+            if ',' in trans:
+                trans = trans.split(',')[0]
+                
+        elif gene_name is not None:
+            transcripts = self.genes.loc[self.genes['Gene name'] == gene_name, 'Protein coding transcripts']
+            canonicals = config.translator[config.translator['Uniprot Canonical'] == 'Canonical']
+            if transcripts.shape[0] > 0:
+                transcripts = transcripts.values[0].split(',')
+                for trans in transcripts:
+                    protein_id = canonicals.loc[canonicals['Transcript stable ID'] == trans, 'UniProtKB/Swiss-Prot ID']
+                    if protein_id.shape[0] !=0:
+                        protein_id = protein_id.values[0]
+                        break
+            else:
+                raise ValueError('Gene name not found')
+
+        seq = self.transcripts.loc[trans, 'Amino Acid Sequence']
+        exons = self.exons.loc[self.exons['Transcript stable ID'] == trans]
+        keep_coding = (exons['Exon Start (Protein)'] != "5' NCR") &  (exons['Exon Start (Protein)'] != "3' NCR") & (exons['Exon Start (Protein)'] != "No coding seq")
+        coding_exons = exons[keep_coding]
+        aa_cuts = coding_exons['Exon Start (Protein)'].astype(float).values
+        ptms = self.ptms[self.ptms['Protein'] == protein_id]
+        numrows = math.ceil(len(seq)/num_aa_per_row)
+        fig, ax = plt.subplots(nrows = numrows, figsize = (figwidth, (1+include_domains*0.5)*numrows))
+        fig.subplots_adjust(hspace = 1)
+        if include_domains:
+            pfam = config.ps_api.get_domains(protein_id, 'pfam')
+        for row in range(numrows):
+            #get where in the protein sequence to start for this row
+            start = row*num_aa_per_row
+            stop = (row+1)*num_aa_per_row
+            ax[row].set_xlim([start,stop])
+            
+            #determine the size of y-axis depending on number of attributes to include
+            num_attributes = 1+include_exons*1+include_domains*1
+            ax[row].set_ylim([0,num_attributes])
+            #check for splice boundaries in the start/stop range
+            splice_bool = np.array([cut < stop and cut >= start for cut in aa_cuts])
+            boundaries = aa_cuts[splice_bool]
+            for bound in boundaries:
+                #color based on ragged or normal boundary
+                if bound % 1 != 0:
+                    c = 'red'
+                else:
+                    c = 'blue'
+                ax[row].plot([bound,bound],[0,1], c= c)
+            for i in range(start, stop):
+                pos = i + 1
+                if pos in ptms['PTM Location (AA)'].values:
+                    c = 'red'
+                else:
+                    c = 'black'
+                #add residue if failed
+                try:
+                    ax[row].text(i+1,0.5,seq[i], c= c)
+                except:
+                    break
+    #            if seq1[i] != seq2[i]:
+    #                ax[row].add_patch(patches.Rectangle((i-0.25,0), 1, 2, alpha = 0.2))
+
+            if include_exons:
+                plot_exons = coding_exons[((coding_exons['Exon Start (Protein)'].astype(float) >= start) & (coding_exons['Exon Start (Protein)'].astype(float) <= stop)) | ((coding_exons['Exon End (Protein)'].astype(float) <= stop) & (coding_exons['Exon End (Protein)'].astype(float) >= start))]
+                for i in plot_exons.index:
+                    exon_start = float(plot_exons.loc[i,'Exon Start (Protein)'])
+                    exon_end = float(plot_exons.loc[i, 'Exon End (Protein)'])
+                    if start > exon_start:
+                        exon_start = start
+                    if stop < exon_end:
+                        exon_end = stop
+                        
+                    #check if exon is constituitive
+                    if plot_exons.loc[i,'Constitutive exon'] == 1:
+                        c = 'plum'
+                    else:
+                        c = 'lightsteelblue'
+                    rect = patches.Rectangle((exon_start, 1.1), exon_end - exon_start, 0.8, edgecolor = 'black', linewidth = 1, facecolor = c)
+                    ax[row].add_patch(rect)
+                    ax[row].text(exon_end - (exon_end-exon_start)/2, 1.5, plot_exons.loc[i, 'Exon stable ID'], c= 'white',ha = 'center', va = 'center')
+                ax[row].text(start-1, 1.5,'Exon stable ID', ha = 'right', va = 'center')
+                
+            if include_domains:
+                height = 1.5+include_exons*1
+                ax[row].axhline(height, zorder = 0, c= 'black')
+                for domain in pfam:
+                    if (int(domain[1]) < start) and int(domain[2]) > stop:
+                        rect = patches.Rectangle((int(start), height-0.4), int(stop)-int(start), 0.8, facecolor = 'white', edgecolor = 'black',linewidth = 1, zorder = 1)
+                        ax[row].add_patch(rect)
+                        ax[row].text(int(stop)-(stop-start)/2, height, domain[0], ha = 'center', va = 'center')
+                    if (int(domain[1]) > start-1 and int(domain[1]) < stop) and (int(domain[2]) > start and int(domain[2]) < stop):
+                        rect = patches.Rectangle((int(domain[1]), height-0.4), int(domain[2])-int(domain[1]), 0.8, facecolor = 'white', edgecolor = 'black',linewidth = 1, zorder = 1)
+                        ax[row].add_patch(rect)
+                        ax[row].text(int(domain[2])-(int(domain[2])-int(domain[1]))/2, height, domain[0], ha = 'center', va = 'center')
+                    elif (int(domain[1]) > start and int(domain[1]) < stop):
+                        rect = patches.Rectangle((int(domain[1]), height-0.4), int(stop)-int(domain[1]), 0.8, facecolor = 'white', edgecolor = 'black',linewidth = 1, zorder = 1) 
+                        ax[row].add_patch(rect)
+                        ax[row].text(stop-(stop-int(domain[1]))/2, height, domain[0], ha = 'center', va = 'center')
+                    elif (int(domain[2]) > start and int(domain[2]) < stop):
+                        rect = patches.Rectangle((start, height-0.4), int(domain[2])-int(start), 0.8, facecolor = 'white', edgecolor = 'black',linewidth = 1, zorder = 1)
+                        ax[row].add_patch(rect)
+                        ax[row].text(int(domain[2])-(int(domain[2])-start)/2, height, domain[0], ha = 'center', va = 'center')
+                ax[row].text(start-1, 1.5+include_domains*1,'Domains', ha = 'right', va = 'center')
+                        
+                
+            ax[row].spines["top"].set_visible(False)
+            ax[row].spines["right"].set_visible(False)
+            ax[row].spines["left"].set_visible(False)
+            ax[row].get_yaxis().set_visible(False)
+
+    fig.suptitle(gene_name)
+        
     def plotPTMs(self, transcript_id):
+        """
+        Currently broken, but idea is that transcript is plotted with location of ptms indicated, highlighting conserved vs. spliced
+        ptms
+        """
         fig, ax = plt.subplots(figsize = (15,2))
 
         ax.set_ylim([0,2])
