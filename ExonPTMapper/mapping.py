acd1b54b9be1d6ac3ba603fa705abe4343861255
srcrowl <sc8wf@virginia.edu>
2022-12-08 09:42:45 -0500

    Add utility mapping functions to check reading frame and get gene coding region

--- ExonPTMapper/mapping.py
+++ ExonPTMapper/mapping.py
@@ -625,6 +625,77 @@ def Prot_to_RNA(pos, cds_start):
     Given residue location, indicate the location of first nucleotide in transcript (first nucleotide in the codon)
     """
     return (int(pos)*3 -3) + int(cds_start)
+    
+    
+def getCodingRegionInGene(exon, transcript, strand):
+    exon_start = int(transcript['Relative CDS Start (bp)']) - int(exon['Exon Start (Transcript)'])
+    exon_stop = int(transcript['Relative CDS Stop (bp)']) - int(exon['Exon Start (Transcript)'])
+    #forward strand
+    if strand == 1:
+        if exon_start < 0:
+            gene_start = exon['Exon Start (Gene)']
+        else:
+            gene_start = exon['Exon Start (Gene)'] + exon_start
+
+        if exon_stop > exon['Exon Length']:
+            gene_stop = exon['Exon End (Gene)']
+        else:
+            gene_stop = exon['Exon Start (Gene)'] + exon_stop
+    #reverse strand
+    else:
+        if exon_start < 0:
+            gene_stop = exon['Exon End (Gene)']
+        else:
+            gene_stop = exon['Exon End (Gene)'] - exon_start
+        if exon_stop > exon['Exon Length']:
+            gene_start = exon['Exon Start (Gene)']
+        else:
+            gene_start = exon['Exon End (Gene)'] - exon_stop
+
+    return gene_start, gene_stop
+
+def checkFrame(exon, transcript, loc, loc_type = 'Gene', strand = 1):
+    """
+    given location in gene, transcript, or exon, return the location in the frame
+    
+    1 = first base pair of codon
+    2 = second base pair of codon
+    3 = third base pair of codon
+    
+    Primary motivation of the function is to determine whether the same gene location in different transcripts is found in the same reading frame
+    
+    Parameters
+    ----------
+    exon: pandas series
+        series object containing exon information for exon of interest
+    transcript: pandas series
+        series object containing transcript information related to exon of interest
+    loc: int
+        location of nucleotide to check where in frame it is located
+    loc_type: string
+        
+    """
+    if loc_type == 'Gene':
+        #calculate location of nucleotide in exon (with 0 being the first base pair of the exon). Consider whether on reverse or forward strand
+        if strand == 1:
+            loc_in_exon = loc - int(exon['Exon Start (Gene)'])
+        else:
+            loc_in_exon = exon['Exon End (Gene)'] - loc
+        #calculate location of nucleotide in transcript (with 0 being the first base pair of the entire transcript, including UTRs)
+        loc_in_transcript = loc_in_exon + int(exon['Exon Start (Transcript)'])
+        #calculate the location in the reading frame (mod returns 0 if multiple of 3 but want this to indicate first bp of a codon, so add 1)
+        frame = (loc_in_transcript - int(transcript['Relative CDS Start (bp)'])) % 3 + 1
+    elif loc_type == 'Exon':
+        #calculate location of nucleotide in transcript (with 0 being the first base pair of the entire transcript, including UTRs)
+        loc_in_transcript = loc + int(exon['Exon Start (Transcript)'])
+        #calculate the location in the reading frame (mod returns 0 if multiple of 3 but want this to indicate first bp of a codon, so add 1)
+        frame = (loc_in_transcript - int(transcript['Relative CDS Start (bp)'])) % 3 + 1
+    elif loc_type == 'Transcript':
+        #calculate the location in the reading frame (mod returns 0 if multiple of 3 but want this to indicate first bp of a codon, so add 1)
+        frame = (loc - int(transcript['Relative CDS Start (bp)'])) % 3 + 1
+    else:
+        print("Invalid loc_type. Can only be based on location in 'Gene','Exon', or 'Transcript'")
+    return frame
 
           
 
