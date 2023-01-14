b8cbde304a35bc7ba166ef8412179ec199a893a0
srcrowl <sc8wf@virginia.edu>
2023-01-14 10:52:28 -0500

    Trimmed unnecesary functions in config and set up autodownload of translator file

--- ExonPTMapper/config.py
+++ ExonPTMapper/config.py
@@ -4,6 +4,7 @@ import json
 import gzip
 import sys
 import os
+import pybiomart
 from Bio import SeqIO
 #import swifter
 
@@ -11,83 +12,11 @@ from Bio import SeqIO
 api_dir = 'C:\\Users\Sam\OneDrive\Documents\GradSchool\Research'
 ps_data_dir = 'C:\\Users\Sam\OneDrive\Documents\GradSchool\Research\ProteomeScoutAPI\proteomescout_mammalia_20220131\data.tsv'
 source_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_Oct182022/ensembl_data/'
-processed_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_Oct182022/processed_data_dir/'
-translator_file = 'uniprot_translator.csv'
+processed_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_Oct182022/test/'
 available_transcripts_file = processed_data_dir + 'available_transcripts.json'
 
-if os.path.isfile(available_transcripts_file):
-    with open(available_transcripts_file, 'r') as f:
-        available_transcripts = json.load(f)
-else:
-    print('Indicated available transcript file does not exist. Run.... \n')
-    available_transcripts = None
-
-#load ProteomeScoutAPI
-sys.path.append(api_dir)
-from ProteomeScoutAPI import proteomeScoutAPI
-ps_api = proteomeScoutAPI.ProteomeScoutAPI(ps_data_dir)
-
-### Utility Functions ###
-def getCanonicalIDs(translator, ID_type = 'Transcript'):
-    """
-    Use translator to extract only the canonical proteins (either Uniprot IDs or Transcript IDs)
-    """
-    if ID_type == 'Transcript':
-        return translator[translator['canonicals']=='canonical']['Transcript stable ID'].to_list()
-    elif ID_type == 'Protein':
-        return translator[translator['canonicals']=='canonical']['Uniprot ID'].to_list()
-    else:
-        print("Please indicate whether you want 'Transcript' or 'Protein' IDs")
-        return None
 
 
-### Processing Functions ###
-def processTranslator(ensembl_translator_dir):
-    #load info to translate between proteomeScout
-    translator = pd.read_csv(ensembl_translator_dir, compression = 'gzip')
-    translator.dropna(inplace = True)
-    #translator= pd.concat([pd.Series(row['Entry'], row['Ensembl transcript'].split(';'))			  
-    #                    for _, row in translator.iterrows()]).reset_index()
-    #translator.columns = ['Transcript and isoform', 'Uniprot ID']
-    #get the transcript ID alone
-    #translator['Transcript stable ID'] = translator.apply(just_transcript, axis = 1)
-    #get the uniprot ID alone
-    #translator['Isoform'] = translator.apply(just_isoform, axis = 1)
-    #remove any empty rows
-    #translator.dropna(inplace = True)
-    #indicate whether the transcript/protein is the canonical protein
-    translator['canonicals'] = translator.apply(is_canonical, axis = 1)
-    return translator
-
-
-def just_transcript(row):
-    """
-    From ensemble translation data file, isolate just the transcript ID
-    """
-    entry= row['Transcript and isoform']
-    if entry == '':
-        transcript = 'N/A'
-    elif len(entry.split()) == 1:
-        transcript = entry
-    else:
-        transcript = entry.split()[0]
-        
-    return transcript
-    
-def just_isoform(row):
-    """
-    From ensemble translation data file, isolate just the Uniprot ID
-    """
-    entry= row['Transcript and isoform']
-    if entry == '':
-        isoform = np.nan
-    elif len(entry.split()) == 1:
-        isoform = '1'
-    else:
-        isoform = entry.split()[1]
-        
-    return isoform
-    
 def is_canonical(row):
     """
     Based on the uniprot ID, label each protein as canonical or isoform
@@ -103,38 +32,54 @@ def is_canonical(row):
         
     return ans
     
-    
-def processEnsemblFasta(file, id_col = 'ID', seq_col = 'Seq'):
-    data_dict = {id_col:[],seq_col:[]}
-    with gzip.open(file,'rt') as handle:
-        for record in SeqIO.parse(handle, 'fasta'):
-            ids = record.id
-            seq = str(record.seq)
-            
-            data_dict[id_col].append(ids)
-            data_dict[seq_col].append(seq)
-            
-    return pd.DataFrame(data_dict)
-    
-def perfect_align(row):
-	
-	u = row['PS Seq']
-	v = row['GENCODE Seq']
-	ans = u==v
-	
-	return ans
+
+
+if os.path.isfile(available_transcripts_file):
+    with open(available_transcripts_file, 'r') as f:
+        available_transcripts = json.load(f)
+else:
+    print('Indicated available transcript file does not exist. Run.... \n')
+    available_transcripts = None
+
+#load ProteomeScoutAPI
+sys.path.append(api_dir)
+from ProteomeScoutAPI import proteomeScoutAPI
+ps_api = proteomeScoutAPI.ProteomeScoutAPI(ps_data_dir)
 
 
 #load uniprot translator dataframe, process if need be
-if os.path.isfile(processed_data_dir + translator_file):
-    translator = pd.read_csv(processed_data_dir + translator_file)
-elif os.path.isfile(source_data_dir + translator_file+'.gz'):
-    translator = pd.read_csv(source_data_dir + translator_file+'.gz')
-    if 'Uniprot Isoform' not in translator.columns:
-        translator['Uniprot Canonical'] = translator.apply(is_canonical, axis =1)
-    translator.to_csv(processed_data_dir + translator_file)
+print('Downloading ID translator file')
+if os.path.isfile(processed_data_dir + 'translator.csv'):
+    translator = pd.read_csv(processed_data_dir + 'translator.csv')
 else:
-    print('Provided translator file does not exist in source data directory or in processed data directory. Update config file.')
+    dataset = pybiomart.Dataset(name='hsapiens_gene_ensembl',
+                   host='http://www.ensembl.org')
+    translator = dataset.query(attributes=['ensembl_gene_id','external_gene_name', 'ensembl_transcript_id',
+                                       'uniprotswissprot', 'uniprot_isoform'],
+             filters = {'biotype':'protein_coding','transcript_biotype':'protein_coding'})
+             
+    #indicate whether row contains information on uniprot canonical transcript
+    translator['Uniprot Canonical'] = translator.apply(is_canonical, axis =1)
+    translator.to_csv(processed_data_dir + 'translator.csv')
+
+
+
+def is_canonical(row):
+    """
+    Based on the uniprot ID, label each protein as canonical or isoform
+    """
+    if row['UniProtKB/Swiss-Prot ID'] is np.nan:
+        ans = np.nan
+    elif row['UniProtKB isoform ID'] is np.nan:
+        ans = 'Canonical'
+    elif row['UniProtKB isoform ID'].split('-')[1] == 1:
+        ans = 'Canonical'
+    else:
+        ans = 'Alternative'
+        
+    return ans
+    
+
 
 
 
