1c1d4537083ef717318e08892d8e8f2c1e9e036f
srcrowl <sc8wf@virginia.edu>
2023-04-24 17:52:29 -0400

    Expanded the ID types downloaded for translator file

--- ExonPTMapper/config.py
+++ ExonPTMapper/config.py
@@ -11,8 +11,8 @@ from Bio import SeqIO
 #update these lines as needed
 api_dir = 'C:\\Users\Sam\OneDrive\Documents\GradSchool\Research'
 ps_data_dir = 'C:\\Users\Sam\OneDrive\Documents\GradSchool\Research\ProteomeScoutAPI\proteomescout_mammalia_20220131\data.tsv'
-source_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_Feb162023/ensembl_data/'
-processed_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_Feb162023/processed_data_dir/'
+source_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_March232023/ensembl_data/'
+processed_data_dir = 'C://Users/Sam/OneDrive/Documents/GradSchool/Research/Splicing/Data_March232023/processed_data_dir/'
 #source_data_dir = '../Ensembl54_Data/ensembl_data/'
 #processed_data_dir = '../Ensembl54_Data/processed_data_dir/'
 available_transcripts_file = processed_data_dir + 'available_transcripts.json'
@@ -63,11 +63,37 @@ else:
                                        'uniprotswissprot', 'uniprot_isoform'],
              filters = {'biotype':'protein_coding','transcript_biotype':'protein_coding',
              'chromosome_name':chromosomes})
-             
+    translator2 = dataset.query(attributes=['ensembl_gene_id','external_gene_name', 'ensembl_transcript_id', 'pdb','refseq_mrna', 'ccds'],
+             filters = {'biotype':'protein_coding','transcript_biotype':'protein_coding',
+             'chromosome_name':chromosomes})
+    translator = translator.merge(translator2, on = ['Gene stable ID', 'Gene name', 'Transcript stable ID'], how = 'outer')
+    translator = translator.drop_duplicates()
     #indicate whether row contains information on uniprot canonical transcript
     translator['Uniprot Canonical'] = translator.apply(is_canonical, axis =1)
+    
     translator.to_csv(processed_data_dir + 'translator.csv')
+    
+if os.path.isfile(processed_data_dir + 'isoforms.csv'):
+    isoforms = pd.read_csv(processed_data_dir + 'isoforms.csv')
 
+
+def getIsoformInfo(transcripts):
+    """
+    Get dataframe where each row corresponds to a unique protein isoform, with transcripts with identitical protein information being in the same row
+    """
+    #add amino acid sequence to translator information
+    merged = config.translator.merge(transcripts['Amino Acid Sequence'], left_on = 'Transcript stable ID', right_index = True)
+    
+    #group information by gene stable ID and amino acid sequence (each unique protein isoform associated with the gene)
+    transcripts =  merged.groupby(['Gene stable ID','Gene name', 'Amino Acid Sequence'])['Transcript stable ID'].apply(set).apply(','.join)
+    proteins = merged.groupby(['Gene stable ID', 'Gene name', 'Amino Acid Sequence'])['UniProtKB/Swiss-Prot ID'].apply(set).apply(lambda x: ','.join(y for y in x if y == y))
+    isoform_id = merged.groupby(['Gene stable ID', 'Gene name', 'Amino Acid Sequence'])['UniProtKB isoform ID'].apply(set).apply(lambda x: ','.join(y for y in x if y == y))
+    canonical = merged.groupby(['Gene stable ID', 'Gene name', 'Amino Acid Sequence'])['Uniprot Canonical'].apply(set).apply(lambda x: ','.join(y for y in x if y == y))
+    
+    #combine into dataframe
+    isoforms = pd.concat([proteins, isoform_id, canonical, transcripts], axis = 1).reset_index()
+    isoforms['Uniprot Canonical'] = isoforms['Uniprot Canonical'].replace('', 'Ensembl Alternative')
+    return isoforms
 
 
 def is_canonical(row):
